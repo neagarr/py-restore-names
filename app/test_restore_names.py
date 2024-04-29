91947da996a2454122046592af39251e1b1219d7
Mykola Kokoiev <veokoknik@gmail.com>
2024-04-29 10:16:07 +0200

    SOL

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,30 @@
 import pytest
+from typing import List
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def data_base() -> List[dict]:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Jack Adams",
+        },
+    ]
+
+    return users
+
+
+def test_restore_names(data_base: List[dict]) -> None:
+    restore_names(data_base)
+
+    for user in data_base:
+        assert user["first_name"] == "Jack"
+
+
+print(type(data_base))
